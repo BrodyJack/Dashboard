aa95b70f27202c2b5bfba37c45852c1f83978afa
Brody Johnstone <brodyjackj@gmail.com>
2018-06-28 23:56:53 -0400

    server files for socket.io stuff

--- dashboard_server/server.js
+++ dashboard_server/server.js
@@ -1,14 +1,33 @@
+const http = require('http');
 const app = require('express')();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('user connected');
+
+  socket.on('message entered', message => {
+    console.log(message);
+    setTimeout(() => {
+      socket.emit('server message', 'ahoy m8e');
+    }, 1500);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
+});
+
 app.set('port', process.env.PORT || 3001);
 
-app.get("/", (req, res) => {
-    console.log('requested /')
-})
+app.get('/', (req, res) => {
+  console.log('requested /');
+});
 
-app.get("/test", (req, res) => {
-    console.log('requested /test')
-})
+app.get('/test', (req, res) => {
+  console.log('requested /test');
+});
 
-app.listen(app.get('port'), () => {
-    console.log(`Running server on: http://localhost:${app.get('port')}/`);
-})
+server.listen(app.get('port'), () => {
+  console.log(`Running server on: http://localhost:${app.get('port')}/`);
+});
