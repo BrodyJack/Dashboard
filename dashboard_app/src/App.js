4023f256609824a31291e6257995c01b3f3b0720
Brandon Manuel <brandon-manuel@live.com>
2018-06-28 22:01:05 -0400

    Basic terminal ul added

--- dashboard_app/src/App.js
+++ dashboard_app/src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    messages: ['Welcome to the meme... type a message to meme.....']
+  };
+
+  addText = event => {
+    if (event.key === 'Enter') {
+      this.setState({
+        text: this.state.messages.push('>  ' + event.target.value)
+      });
+      event.target.value = '';
+    }
+  };
+
+  displayMessage = (message, index) => {
+    return <li key={index}>{message}</li>;
+  };
   render() {
     fetch('/test');
 
@@ -12,9 +28,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="terminalDiv">
+          <ul className="terminalList">
+            {this.state.messages.map(this.displayMessage)}
+          </ul>
+        </div>
+        <span className="terminalInput">
+          > <input className="terminalInput" onKeyPress={this.addText} />
+        </span>
       </div>
     );
   }
